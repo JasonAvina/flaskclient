001380c9e7b2422577be3237c51dd9faf807d217
Jason Avina <5945015+JasonAvina@users.noreply.github.com>
2022-12-06 17:09:00 +0000

    updated client

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ class Client:
         Returns server's response converted back from JSON to a Python object."""
         json_string = json.dumps(python_obj)
         json_bytes = json_string.encode('utf-8')
-        request = Request(  #instantiation of an object representing a url/http request
+        request = Request(  # instantiation of an object representing a url/http request
             url=url,
-            method='GET',  #get request here is the client getting info from a server
+            method='GET',  # get request here is the client getting info from a server
             headers={
                 'Content-Length': len(json_bytes),
                 'Content-Type': 'application/json',
             },
-            data=json_bytes  #this is the message body with whatever data python_obj parameter held
+            data=json_bytes  # this is the message body with whatever data python_obj parameter held
         )
         response = urlopen(request)
         if 200 <= response.status <= 299:
@@ -41,16 +41,16 @@ if __name__ == '__main__':
 
     # TODO: Set your base server url according to how you are running your server.
     base_server_url = "https://flaskserver.jasonavina.repl.co"
-    intended_path = "/lastclose"
-    #querystring = "?ticker=msft"
-    full_server_url = base_server_url + intended_path #+ querystring
+    intended_path = "/alltimeseries"
+    # querystring = "?ticker=msft"
+    full_server_url = base_server_url + intended_path  # + querystring
 
     # TODO: Create the data you want to send.
-    
-    sample_data_to_send = {"data": {'snp':'up', 'qqq':'down'}}  # Any combination of JSON-compatible Python objects: int, float, str, list, dict, True/False/None
+
+    sample_data_to_send = json.dumps('MSFT')  # Any combination of JSON-compatible Python objects: int, float, str, list, dict, True/False/None
 
     result = Client.send_json(full_server_url, sample_data_to_send)
-  
+
     if result is Client.FAILED_REQUEST:
         print(f"REQUEST FAILED for {sample_data_to_send=}")
     else:
